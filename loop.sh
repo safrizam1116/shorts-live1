cf602685ad2e986132872009ba8dda57e8fc6323
safrizam1116 <safrizam1116@gmail.com>
2025-07-12 23:51:23 +0700

    Create loop.sh

---
+++ loop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "🔁 Starting 9:16 loop live stream..."
+
+ffmpeg -re -stream_loop -1 -i video.mp4 \
+-c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k \
+-c:a aac -b:a 128k -ar 44100 -f flv "rtmp://a.rtmp.youtube.com/live2/$STREAM_KEY"
