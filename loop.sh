66fe3c0d2e80f753d777c999576860f0807aa051
safrizam1116 <safrizam1116@gmail.com>
2025-07-13 00:48:33 +0700

    Delete loop.sh

--- loop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "🔁 Starting 9:16 loop live stream..."
-
-ffmpeg -re -stream_loop -1 -i live1.mp4 \
--c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k \
--c:a aac -b:a 128k -ar 44100 -f flv "rtmp://a.rtmp.youtube.com/live2/$STREAM_KEY"
